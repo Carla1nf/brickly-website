3efff2af2702506c8aaa62e0ac975adfed4bc2a6
Lopl <carlanft1@gmail.com>
2023-12-30 19:29:00 -0300

    small

--- components/form.tsx
+++ components/form.tsx
@@ -25,6 +25,7 @@ export default function Form () {
      setName("");
      setLastname("");
      setIsSubmitting(false);
+     setIsSuccess(true);
      }
 
 
@@ -34,7 +35,7 @@ export default function Form () {
         <form onSubmit={handleSubmit} className="flex flex-col gap-y-5">
             <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="E-mail" type="email" className=" px-4 py-4 rounded-xl bg-gray-100 animate-enter-token delay-100 fill-mode-forwards opacity-0"/>
             <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Nombre" className=" px-4 py-4 rounded-xl bg-gray-100 animate-enter-token delay-200 fill-mode-forwards opacity-0"/>
-            <input value={lastname}onChange={(e) => setLastname(e.target.value)} placeholder="Apellido" className=" px-4 py-4 rounded-xl bg-gray-100 animate-enter-token delay-300 fill-mode-forwards opacity-0"/>
+            <input value={lastname} onChange={(e) => setLastname(e.target.value)} placeholder="Apellido" className=" px-4 py-4 rounded-xl bg-gray-100 animate-enter-token delay-300 fill-mode-forwards opacity-0"/>
             {success ? 
                 <Button className="bg-green-400 font-semibold rounded-lg text-white h-10  cursor-pointer transition-all" color="tomato" type="submit" variant='soft' size='3' onClick={() => handleSubmit()} disabled={isSubmitting}>¡Revisa tu e-mail! </Button> : 
                 <Button className="bg-black font-semibold rounded-lg text-white h-10  cursor-pointer transition-all" color="tomato" type="submit" variant='soft' size='3' onClick={() => handleSubmit()} disabled={isSubmitting}>Pre-registrarse</Button>
